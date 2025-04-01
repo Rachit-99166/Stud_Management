cae30ab3cd44261c962ca976ff5ac2dd9703d2d1
Rachit <rachitdua997@gmail.com>
2025-04-02 00:57:55 +0530

    Update ApiController.java

--- Springapp/src/main/java/com/examly/springapp/controller/ApiController.java
+++ Springapp/src/main/java/com/examly/springapp/controller/ApiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examly.springapp.model.Student;
-import com.examly.springapp.service.Studentservice;
+import com.examly.springapp.service.Studentservice; 
 
 @RestController
 public class ApiController {
